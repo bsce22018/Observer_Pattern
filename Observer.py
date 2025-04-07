9c88c8fc09eed9b39aee80a46f3b88b96b4a6e8a
Evibes Development <evibessolutions@gmail.com>
2025-04-07 12:19:38 +0500

    Update Observer.py

--- Observer.py
+++ Observer.py
@@ -37,6 +37,13 @@ class AnalyticsDashboard(Observer):
     def update(self, student_name, grade):
         print(f"[Analytics Dashboard] Logging: {student_name} got {grade}.")
 
+# Concrete Observer: Teacher Notifier
+class TeacherNotifier(Observer):
+    def update(self, student_name, grade):
+        print(f"[Teacher Notifier] Notification: {student_name} received {grade}. Please review if necessary.")
+
+
+
 # Usage
 if __name__ == "__main__":
     gradebook = GradeBook()
@@ -44,10 +51,12 @@ if __name__ == "__main__":
     parent = ParentPortal()
     student = StudentPortal()
     dashboard = AnalyticsDashboard()
+    teacher = TeacherNotifier()
 
     gradebook.add_observer(parent)
     gradebook.add_observer(student)
     gradebook.add_observer(dashboard)
+    gradebook.add_observer(teacher) 
 
     gradebook.enter_grade("Alice", 92)
     gradebook.enter_grade("Bob", 85)
